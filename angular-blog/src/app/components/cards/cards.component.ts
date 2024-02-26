894f52fd5f81c9a459df4c1722f841d57f46796e
PlopesK <popi081004@hotmail.com>
2024-02-26 08:08:57 -0300

    Base project finished

--- angular-blog/src/app/components/cards/cards.component.ts
+++ angular-blog/src/app/components/cards/cards.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.css']
+  styleUrls: ['./cards.component.css'],
 })
-export class CardsComponent {
-
-}
+export class CardsComponent {}
--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -3,28 +3,28 @@ export const dataFake = [
     id: '1',
     photoCover:
       'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg',
-    title: 'title',
+    title: 'title 1',
     description: 'description',
   },
   {
     id: '2',
     photoCover:
       'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg',
-    title: 'title',
+    title: 'title 2',
     description: 'description',
   },
   {
     id: '3',
     photoCover:
       'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg',
-    title: 'title',
+    title: 'title 3',
     description: 'description',
   },
   {
     id: '4',
     photoCover:
       'https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg',
-    title: 'title',
+    title: 'title 4',
     description: 'description',
   },
 ];
